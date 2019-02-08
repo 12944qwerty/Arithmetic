32c6a38f9840f70707f5515f65c7bda4d12f2a68
12944qwerty <45984786+12944qwerty@users.noreply.github.com>
2019-02-08 09:44:57 -0600

    Create math.py

---
+++ Arithmetic/math.py
@@ -0,0 +1,19 @@
+import math
+
+def multiply(x,y):
+  return x * y
+
+def add(x,y):
+  return x + y
+
+def divide(x,y):
+  return x / y
+
+def subtract(x,y):
+  return x - y
+
+def power(x,y):
+  return x ** y
+
+def root(x):
+  return math.sqrt(x)
