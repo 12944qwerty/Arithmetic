ce90a5680612bf78433d8d3739caeaccee56315f
12944qwerty <qwerty12944qwerty@gmail.com>
2019-02-27 16:03:59 -0600

    math.py

--- Arithmetic/math.py
+++ Arithmetic/math.py
@@ -1,20 +1,37 @@
-import math
-
 class Math(object):
-  def multiply(self,x,y):
-    return x * y
+    def multiply(self,x,y):
+        return x * y
 
-  def add(self,x,y):
-    return x + y
+    def add(self,x,y):
+        return x + y
 
-  def divide(self,x,y):
-    return x / y
+    def divide(self,x,y):
+        return x / y
 
-  def subtract(self,x,y):
-    return x - y
+    def subtract(self,x,y):
+        return x - y
 
-  def power(self,x,y):
-    return x ** y
+    def power(self,x,y):
+        return x ** y
 
-  def root(x):
-    return math.sqrt(self,x)
+    def root(self,x,y):
+        """Args: Numb, Root. Number is the number in radical. Root is like cuberoot or sqrt but as int."""
+        i = 0
+        while i < x:
+            if i ** y == x:
+                return i
+                break
+            i += 1
+
+math = Math()
+
+class Algebra(object):
+    def zeros(self,a,b,c):
+        """Gets the zeros of a quadratic function. Args are A, B, and C in form 'Ax^2 + Bx + C'."""
+        first = (-b + int(math.root(b ** 2 - (4*a*c),2)))/(2 * a)
+        second = (-b - int(math.root(b ** 2 - (4*a*c),2)))/(2* a)
+        return str(first) + ', ' + str(second)
+
+algebra = Algebra()
+
+print(algebra.zeros(1,1,1))
