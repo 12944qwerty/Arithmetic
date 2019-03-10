b08850ef9d9b808f626e45fc12dc475493599365
12944qwerty <qwerty12944qwerty@gmail.com>
2019-03-09 20:08:36 -0600

    cleaner

---
+++ Arithmetic/algebra.py
@@ -0,0 +1,13 @@
+import math
+
+class Algebra(object):
+    def zeros(self,a,b,c):
+        """Gets the zeros of a quadratic function. Args are A, B, and C in form 'Ax^2 + Bx + C'."""
+        first = (-b + math.sqrt(b ** 2 - (4*a*c)))/(2 * a)
+        second = (-b - math.sqrt(b ** 2 - (4*a*c)))/(2* a)
+        return str(first) + ', ' + str(second)
+
+
+alg = Algebra()
+
+print(alg.zeros(1,0,0))
